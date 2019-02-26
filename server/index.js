e5cf3b1a04df6601f988da4a08a3021dd2cfdb67
Adelinestack <adeline.colombo@stackerine.com>
2019-02-26 15:24:20 +0100

    server get folder content

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+app.use('/', express.static('../client/build/'));
+
+const contentByPath = elementPath => {
+  return fs.readdirSync(`./img${elementPath}`, { withFileTypes: true });
+};
+
+app.get('/images', (req, res) => {
+  const elementPath = req.query.elementPath;
+  const readElement = contentByPath(elementPath);
+  const content = readElement.map(element => ({
+    name: element.name,
+    isDir: element.isDirectory(),
+  }));
+  res.send(content);
+});
 
 const server = app.listen(4000, (req, res) => {
   console.log(`server started on ${server.address().port}`);
