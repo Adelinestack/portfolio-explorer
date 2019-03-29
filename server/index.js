5e0b09bd3b7d980cdfad97afad204ba3f8d5ce79
Adelinestack <adeline.colombo@stackerine.com>
2019-03-29 16:26:28 +0100

    server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const { getContentByPath } = require('./utils/content');
 
-app.use('/', express.static('../client/build/'));
-app.use('/images', express.static('./img'));
+app.use('/img', express.static('./img'));
 
-const contentByPath = elementPath => {
-  return fs.readdirSync(`./img${elementPath}`, { withFileTypes: true });
-};
-
-app.get('/images', (req, res) => {
-  const elementPath = req.query.elementPath;
-  const readElement = contentByPath(elementPath);
-  const content = readElement.map(element => ({
-    name: element.name,
-    isDir: element.isDirectory(),
-    elementPath: `/images${elementPath}${element.name}`,
-  }));
-  console.log(content);
-  res.send(content);
+app.get('/getImages', (req, res) => {
+  const elementPath = req.query.elementPath.substring(1);
+  const content = getContentByPath(`./img/${elementPath}`);
+  res.json(content);
 });
+
+app.use('/*.js', express.static('../client/build/'));
 
 const server = app.listen(4000, (req, res) => {
   console.log(`server started on ${server.address().port}`);
---
+++ server/utils/content.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const getAllImages = (folders, folderContent, path) => {
+  const formattedPath = path.replace(/^\./, '');
+  if (!folders.length) {
+    return folderContent.map(({ name }) => ({
+      path: `${formattedPath}/${name}`,
+      name,
+    }));
+  }
+  return folderContent.map(element => {
+    if (element.isDirectory()) {
+      const newFolderContent = fs.readdirSync(`${path}/${element.name}`, {
+        withFileTypes: true,
+      });
+      const newFolders = newFolderContent
+        .filter(element => element.isDirectory())
+        .map(folder => ({
+          name: folder.name,
+          path: `${element.name}/${folder.name}`,
+        }));
+
+      return getAllImages(
+        newFolders,
+        newFolderContent,
+        `${path}/${element.name}`
+      );
+    }
+    return {
+      path: `${formattedPath}/${element.name}`,
+      name: element.name,
+    };
+  });
+};
+
+const getContentByPath = path => {
+  const folderContent = fs.readdirSync(path, { withFileTypes: true });
+
+  const completePath = path.replace(/^\.\/img\//g, '');
+  const folders = folderContent
+    .filter(element => element.isDirectory())
+    .map(folder => ({
+      name: folder.name,
+      path: `${completePath}/${folder.name}`,
+    }));
+
+  const allImages = _.flattenDeep(getAllImages(folders, folderContent, path));
+
+  return { folders, allImages };
+};
+
+module.exports = { getContentByPath };
